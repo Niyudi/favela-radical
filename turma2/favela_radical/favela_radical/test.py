31658863ad0b5e6c65c92c8d05c084c2783ea47e
Pedro Yudi Honda <niyudi.honda@gmail.com>
2021-09-03 17:42:55 -0300

    aula 1 turma 2

---
+++ turma2/favela_radical/favela_radical/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#                dormir
+from time import sleep
+
+class Personagem(object):
+    def __init__(self):
+        self.posição = 10.0
+        self.velocidade = 3.0
+    
+    def update(self, tempo, aceleração):
+        self.posição = self.posição + (self.velocidade * tempo)
+        self.velocidade = self.velocidade + (aceleração * tempo)
+        
+        if (self.posição < 0):
+            self.posição = 0
+            self.velocidade = -self.velocidade
+
+personagem = Personagem()
+
+tempo = 0.01
+gravidade = -5.0
+
+i = 1
+while (True):
+    personagem.update(tempo, gravidade)
+    
+    print("Update número " + str(i))
+    print(personagem.posição)
+    print(personagem.velocidade)
+    print("")
+    
+    sleep(tempo)
+    
+    i = i + 1
