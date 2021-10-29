e160923b288e5359633c53b4fd016ed311a206f4
Pedro Yudi Honda <niyudi.honda@gmail.com>
2021-10-29 17:30:25 -0300

    aula 29/10

--- turma1/favela_radical/favela_radical/interface.py
+++ turma1/favela_radical/favela_radical/interface.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import (QLabel, QMainWindow, QVBoxLayout,
-                             QWidget)
+from PyQt5.QtWidgets import (QLabel, QMainWindow, QPushButton,
+                             QVBoxLayout, QWidget)
 
 class JanelaPrincipal(QMainWindow):
     def __init__(self):
@@ -15,17 +15,24 @@ class JanelaPrincipal(QMainWindow):
         
         self.iniciarImagem()
     
+    def mudarTexto(self):
+        print("PRESSIONADO!")
+    
     def iniciarImagem(self):
         self.setWindowTitle(self.título)
         self.resize(self.altura, self.largura)
         
-        texto1 = QLabel("Olá!") #
+        texto1 = QLabel("Olá!")
         
-        layout1 = QVBoxLayout() #
+        botão1 = QPushButton("Botão!")
+        botão1.clicked.connect(self.mudarTexto)
+        
+        layout1 = QVBoxLayout()
         
         layout1.addWidget(texto1)
+        layout1.addWidget(botão1)
         
-        central_widget = QWidget() #
+        central_widget = QWidget()
         central_widget.setLayout(layout1)
         self.setCentralWidget(central_widget)
         
